77f998b868abe9efd773d4238a2091d530550f33
Micah Britton <micahwkb@gmail.com>
2016-11-03 19:44:54 -0400

    loadedDie working, inserted simple per-use iteration callback

--- loadedDie.js
+++ loadedDie.js
@@ -1,6 +1,11 @@
 var loadedDie = (function () {
+  var count = -1;
   var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
-  /* your code here */
+  return function() {
+    count ++;
+    return list[count];
+  }
+
 })();
 
 console.log(loadedDie());  // 5
