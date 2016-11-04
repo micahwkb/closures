edb26a75a604b0b1ae086af14b6a114b70667794
Micah Britton <micahwkb@gmail.com>
2016-11-03 20:15:06 -0400

    loadedDie now repeats for much longer, built shell for countdownGenerator

--- loadedDie.js
+++ loadedDie.js
@@ -1,8 +1,14 @@
 var loadedDie = (function () {
+  var repeatCount = 1;
+  // repeatCount gives appearance of randomness when rolled 10+ times
   var count = -1;
   var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
   return function() {
-    count ++;
+    if (count === 9) { // so it never runs out!
+      count = repeatCount;
+      repeatCount ++;
+      }
+    else count ++;
     return list[count];
   }
 
@@ -11,3 +17,5 @@ var loadedDie = (function () {
 console.log(loadedDie());  // 5
 console.log(loadedDie());  // 4
 console.log(loadedDie());  // 6
+
+
